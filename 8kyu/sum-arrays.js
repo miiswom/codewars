eb6cd73b15dacc7cb733ea5dc2f7aed86f7980f6
Farmata Kane <kane.farmata@gmail.com>
2024-04-02 01:26:29 +0000

    Add Sum Arrays challenge

---
+++ 8kyu/sum-arrays.js
@@ -0,0 +1,16 @@
+// // Sum Numbers
+// function sum (numbers) {
+//   "use strict";
+// }
+
+// My solution 
+
+function sum (numbers) {
+  "use strict";
+  let total = 0; 
+  
+  for(let i=0; i < numbers.length; i++) {
+  total +=numbers[i] 
+  }
+    return total; 
+};
