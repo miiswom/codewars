c048231aa1ff3574c017c4e58c729d502e118cbb
Farmata Kane <kane.farmata@gmail.com>
2024-10-05 22:10:29 +0100

    Complete #2 hof

--- higher-order-functions/index.js
+++ higher-order-functions/index.js
@@ -10,3 +10,15 @@ function countDevelopers(list) {
   }
   return devCount
 }
+
+/* Coding Meetup #2 - Higher-Order Functions Series - Greet developers */
+
+function greetDevelopers(list) {
+  // thank you for checking out my kata :)
+  for(let dev in list) {
+    if(list[dev].greeting === undefined) {
+     list[dev].greeting = `Hi ${list[dev].firstName}, what do you like the most about ${list[dev].language}?`
+    }
+  }
+  return list
+}
